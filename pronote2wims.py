d13f43ac413f41f990a54496798a41d1e4f77a9b
cspaier <clement.spaier@gmail.com>
2020-03-28 17:53:00 +0100

    premier commit

---
+++ pronote2wims.py
@@ -0,0 +1,27 @@
+from flask import Flask, flash, redirect, request, render_template
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+    if request.method == "GET":
+        return render_template('pronote2wims.html')
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            # on fait les trucs
